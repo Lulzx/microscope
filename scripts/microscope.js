b53f019c701958c4f23518c181536a54a3134c72
worc <entiat.blues@gmail.com>
2016-01-16 14:23:11 -0800

    stub out templating, json object.

---
+++ scripts/microscope.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var Microscope = {};
+
+Microscope.ajax = {};
+
+Microscope.ajax.get = function(url) {
+    var request = new XMLHttpRequest();
+
+    request.open("GET", url, true);
+    request.send();
+
+    return {
+        done: function(callback) {
+            request.onreadystatechange = function() {
+                if (request.readyState === XMLHttpRequest.DONE && request.status === 200) {
+                    callback(request);
+                }
+            }
+        }
+    }
+};
+
+Microscope.ajax.get("first_game.json").done(function(data) {
+    var json = JSON.parse(data.responseText);
+    Microscope.ajax.get("templates/main.html").done(function(data) {
+        var renderedData = render(json, data.responseText);
+        document.body.innerHTML = renderedData;
+    });
+});
+
+function render(json, html) {
+    return Handlebars.compile(html)(json);
+}
