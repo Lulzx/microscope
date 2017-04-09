0925792bba14897eb403ce13167e771542c0735c
worc <curtis.s.mcallister@gmail.com>
2017-04-09 16:23:48 -0700

    render the stub react application inside a StaticRouter for every url

--- server.jsx
+++ server.jsx
@@ -1,11 +1,19 @@
+import React from "react";
 import express from "express";
+import { StaticRouter } from "react-router-dom";
+
+import renderPage from "./templates/index";
+import App from "./app/App";
 
 const app = express();
 
-app.get("*", (req, res) => {
+app.use("*", (req, res) => {
   const title = "Microscope";
-
-  res.status(200).send(title);
+  res.status(200).send(renderPage(title, (
+    <StaticRouter context={{}} location={req.url}>
+      <App />
+    </StaticRouter>
+  )));
 });
 
 export default app;
