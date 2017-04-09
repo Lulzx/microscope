cbc4edc7e6576b993a3f0445dae2d4a9d34d649d
worc <curtis.s.mcallister@gmail.com>
2017-04-09 16:23:15 -0700

    create a top-level for the react application

---
+++ app/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const style = {
+  fontFamily: "OpenSans, sans-serif"
+};
+
+export default () => (
+  <div style={ style }>
+    <div>
+      Hello Microscope
+    </div>
+  </div>
+)
